bd7ec3dfc134ea51ceeebfd0db8494af8dd96516
djalexkidd <alexpyr@outlook.fr>
2023-02-27 15:08:03 +0100

    Jacky ta 4L elle est pourrie

--- init.sql
+++ init.sql
@@ -8,7 +8,16 @@ USE godot;
 
 CREATE TABLE `leaderboard` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `score` int(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NOT NULL,
+  `score` bigint(20) DEFAULT NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- godot.users definition
+
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NOT NULL,
+  `password_digest` varchar(100) DEFAULT NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
